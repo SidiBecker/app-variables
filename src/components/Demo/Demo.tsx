b6844c5ce817cc7425db443a52fd28f4d425c7c2
Sidi Becker <sidibecker@hotmail.com>
2021-05-26 09:45:46 -0300

    pages

--- src/components/Demo/Demo.tsx
+++ src/components/Demo/Demo.tsx
@@ -1,76 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Home } from '../Home/Home';
 import './Demo.scss';
-import {
-  FaUserFriends,
-  FaBullhorn,
-  FaPlusCircle,
-  FaGift,
-  FaCheckCircle,
-  FaSearch,
-  FaCreditCard,
-} from 'react-icons/fa';
 
 export const Demo = () => {
+  const [page, setPage] = useState(0);
+
   return (
     <div className="demo">
-      <div className="phone-container">
-        <div className="logo">
-          <span>LOGOTIPO</span>
+      <div className="demo-area">
+        <div className="pages-select">
+          <button
+            className={page === 0 ? 'selected' : ''}
+            onClick={() => setPage(0)}
+          >
+            Home
+          </button>
+          <button
+            className={page === 1 ? 'selected' : ''}
+            onClick={() => setPage(1)}
+          >
+            Login
+          </button>
         </div>
-        <div className="login">
-          <div className="circle">
-            <FaUserFriends size={60} color={`var(--outline-color-button)`} />
-          </div>
-          <div className="button">
-            <span>ENTRAR</span>
-          </div>
-          <div className="welcome-text">
-            <span>OLÁ, FULANO(A)</span>
-          </div>
-        </div>
-        <div className="external-apps">
-          <div className="external-app">
-            <div className="external-app-container">
-              <FaBullhorn size={40} color={`var(--outline-color-button)`} />
-              <span>ENCARTE</span>
-            </div>
-          </div>
-          <div className="external-app">
-            <div className="external-app-container">
-              <FaPlusCircle size={40} color={`var(--outline-color-button)`} />
-              <span>CONTAS</span>
-            </div>
-          </div>
-        </div>
-        <div className="footer-apps">
-          <div className="apps-label">APLICATIVOS</div>
-          <div className="apps-list">
-            <div className="footer-app">
-              <span className="new-bullet">Novo</span>
-              <div className="footer-app-container">
-                <FaGift size={30} color={`var(--outline-color)`} />
-                <span>SORTEIOS</span>
-              </div>
-            </div>
-            <div className="footer-app">
-              <div className="footer-app-container">
-                <FaCheckCircle size={30} color={`var(--outline-color)`} />
-                <span>PROMOÇÕES</span>
-              </div>
-            </div>
-            <div className="footer-app">
-              <div className="footer-app-container">
-                <FaSearch size={30} color={`var(--outline-color)`} />
-                <span>PREÇO</span>
-              </div>
-            </div>
-            <div className="footer-app">
-              <div className="footer-app-container">
-                <FaCreditCard size={30} color={`var(--outline-color)`} />
-                <span>FIDELIDADE</span>
-              </div>
-            </div>
-          </div>
+
+        <div className="phone-container">
+          {page === 0 && <Home />}
+          {page === 1 && <div>TESTE</div>}
         </div>
       </div>
     </div>
